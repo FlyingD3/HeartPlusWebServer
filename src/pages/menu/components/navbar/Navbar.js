5ff44a20b36b7078aefb232dacbf41a39bda874a
Yurcek <yurig1999@hotmail.it>
2021-07-13 23:30:29 +0200

    fixed navbar overflow

--- src/pages/menu/components/navbar/Navbar.js
+++ src/pages/menu/components/navbar/Navbar.js
@@ -7,35 +7,36 @@ import Logo from "./icons/logo.svg";
 class Navbar extends Component {
   render() {
     return (
-      <header class="header-cnt">
-        <div class="nav-logo">
+      <>
+      <header className="header-cnt">
+        <div className="nav-logo">
           <img src={Logo} alt="logo icon" />
           <a href="/">HeartPlus</a>
         </div>
-        <div class="nav-link-cnt">
-          <div class="nav-link">
+        <div className="nav-link-cnt">
+          <div className="nav-link">
             <Link to="/browser">
               <a>Check</a>
             </Link>
           </div>
-          <div class="nav-link">
+          <div className="nav-link">
             <Link to="/status">
               <a>Status</a>
             </Link>
           </div>
-          <div class="nav-link">
+          <div className="nav-link">
             <Link to="/about">
               <a>About</a>
             </Link>
           </div>
-          <div class="nav-btn-link">
+          <div className="nav-btn-link">
             <Link to="/login">
               <a href="something" className="button6">
                 Login
               </a>
             </Link>
           </div>
-          <div class="nav-btn-link">
+          <div className="nav-btn-link">
             <Link to="/sign-up">
               <a href="something" className="button6">
                 Sign Up
@@ -43,10 +44,30 @@ class Navbar extends Component {
             </Link>
           </div>
         </div>
-        <div className="nav-burger-cnt">
-            
-        </div>
       </header>
+      <div className="nav-burger-cnt">
+          <ul className="navbar" id="navbar">
+            <li>
+              <a href="#">Check</a>
+            </li>
+            <li>
+              <a href="#">Status</a>
+            </li>
+            <li>
+              <a href="#">About</a>
+            </li>
+            <a className="close" href="#">
+              <img
+                src="https://ljc-dev.github.io/testing0/ham-close.svg"
+                alt="close"
+              />
+            </a>
+          </ul>
+          <a className="hamburger" href="#navbar">
+            <img src="https://ljc-dev.github.io/testing0/ham.svg" alt="menu" />
+          </a>
+        </div>
+      </>
     );
   }
 }
